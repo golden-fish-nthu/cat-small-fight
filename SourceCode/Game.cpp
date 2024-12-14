c61d78c4c67ef5d6e7ac8d4ba0b2ed77cac372b3
golden-fish-nthu <josh101.tw@gapp.nthu.edu.tw>
2024-12-15 01:32:00 +0800

    多家勝利失敗條件

--- SourceCode/Game.cpp
+++ SourceCode/Game.cpp
@@ -1,4 +1,7 @@
 #include "Game.h"
+#include "Level.h"
+#include "Player.h"
+#include "Utils.h"
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
@@ -6,9 +9,6 @@
 #include <allegro5/allegro_ttf.h>
 #include <cstring>
 #include <vector>
-#include "Level.h"
-#include "Player.h"
-#include "Utils.h"
 // #include "animal/animal.h"
 #include "data/DataCenter.h"
 #include "data/FontCenter.h"
@@ -29,60 +29,61 @@ constexpr char background_sound_path[] = "./assets/sound/BackgroundMusic.ogg";
  * 对于计时器事件，只有当当前是计时器时才会调用 game_update 和 game_draw 函数。
  */
 void Game::execute() {
-    DataCenter* DC = DataCenter::get_instance();
+    DataCenter *DC = DataCenter::get_instance();
     // 主游戏循环
     bool run = true;
     while (run) {
         // 在此处理所有事件
         al_wait_for_event(event_queue, &event);
         switch (event.type) {
-            case ALLEGRO_EVENT_TIMER: {
-                run &= game_update();
-                game_draw();
-                break;
-            }
-            case ALLEGRO_EVENT_DISPLAY_CLOSE: {  // 停止游戏
-                run = false;
-                break;
-            }
-            case ALLEGRO_EVENT_KEY_DOWN: {
-                DC->key_state[event.keyboard.keycode] = true;
-                // 處理數字鍵
-                if (event.keyboard.keycode >= ALLEGRO_KEY_1 && event.keyboard.keycode <= ALLEGRO_KEY_4) {
-                    int monster_type = event.keyboard.keycode - ALLEGRO_KEY_1;
-                    int monster_cost = 50;  // 假設每個怪物的成本是 50 金幣
-                    printf("monster_cost: %d\n", monster_cost);
-                    if (DC->player->coin >= monster_cost) {
-                        DC->player->coin -= monster_cost;
-                        DC->monsters.emplace_back(Monster::create_monster(static_cast<MonsterType>(monster_type), DC->level->get_road_path(), true));
-                        debug_log("Generated monster type %d\n", monster_type);
-                        debug_log("Player's coin: %d\n", DC->player->coin);
-                    } else {
-                        debug_log("Player's coin: %d\n", DC->player->coin);
-                        debug_log("Not enough money to generate monster type %d\n", monster_type);
-                    }
+        case ALLEGRO_EVENT_TIMER: {
+            run &= game_update();
+            game_draw();
+            break;
+        }
+        case ALLEGRO_EVENT_DISPLAY_CLOSE: { // 停止游戏
+            run = false;
+            break;
+        }
+        case ALLEGRO_EVENT_KEY_DOWN: {
+            DC->key_state[event.keyboard.keycode] = true;
+            // 處理數字鍵
+            if (event.keyboard.keycode >= ALLEGRO_KEY_1 && event.keyboard.keycode <= ALLEGRO_KEY_4) {
+                int monster_type = event.keyboard.keycode - ALLEGRO_KEY_1;
+                int monster_cost = 50; // 假設每個怪物的成本是 50 金幣
+                printf("monster_cost: %d\n", monster_cost);
+                if (DC->player->coin >= monster_cost) {
+                    DC->player->coin -= monster_cost;
+                    DC->monsters.emplace_back(Monster::create_monster(static_cast<MonsterType>(monster_type),
+                                                                      DC->level->get_road_path(), true));
+                    debug_log("Generated monster type %d\n", monster_type);
+                    debug_log("Player's coin: %d\n", DC->player->coin);
+                } else {
+                    debug_log("Player's coin: %d\n", DC->player->coin);
+                    debug_log("Not enough money to generate monster type %d\n", monster_type);
                 }
-                break;
             }
-            case ALLEGRO_EVENT_KEY_UP: {
-                DC->key_state[event.keyboard.keycode] = false;
-                break;
-            }
-            case ALLEGRO_EVENT_MOUSE_AXES: {
-                DC->mouse.x = event.mouse.x;
-                DC->mouse.y = event.mouse.y;
-                break;
-            }
-            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN: {
-                DC->mouse_state[event.mouse.button] = true;
-                break;
-            }
-            case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
-                DC->mouse_state[event.mouse.button] = false;
-                break;
-            }
-            default:
-                break;
+            break;
+        }
+        case ALLEGRO_EVENT_KEY_UP: {
+            DC->key_state[event.keyboard.keycode] = false;
+            break;
+        }
+        case ALLEGRO_EVENT_MOUSE_AXES: {
+            DC->mouse.x = event.mouse.x;
+            DC->mouse.y = event.mouse.y;
+            break;
+        }
+        case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN: {
+            DC->mouse_state[event.mouse.button] = true;
+            break;
+        }
+        case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
+            DC->mouse_state[event.mouse.button] = false;
+            break;
+        }
+        default:
+            break;
         }
     }
 }
@@ -92,7 +93,7 @@ void Game::execute() {
  * @details 只创建一个计时器，因为游戏及其所有数据都应该同步处理。
  */
 Game::Game() {
-    DataCenter* DC = DataCenter::get_instance();
+    DataCenter *DC = DataCenter::get_instance();
     GAME_ASSERT(al_init(), "failed to initialize allegro.");
 
     // 初始化 Allegro 插件
@@ -112,15 +113,9 @@ Game::Game() {
     GAME_ASSERT(event_init, "failed to initialize allegro events.");
 
     // 初始化游戏主体
-    GAME_ASSERT(
-        display = al_create_display(DC->window_width, DC->window_height),
-        "failed to create display.");
-    GAME_ASSERT(
-        timer = al_create_timer(1.0 / DC->FPS),
-        "failed to create timer.");
-    GAME_ASSERT(
-        event_queue = al_create_event_queue(),
-        "failed to create event queue.");
+    GAME_ASSERT(display = al_create_display(DC->window_width, DC->window_height), "failed to create display.");
+    GAME_ASSERT(timer = al_create_timer(1.0 / DC->FPS), "failed to create timer.");
+    GAME_ASSERT(event_queue = al_create_event_queue(), "failed to create event queue.");
 
     debug_log("Game initialized.\n");
     game_init();
@@ -130,10 +125,10 @@ Game::Game() {
  * @brief 初始化所有辅助资源。
  */
 void Game::game_init() {
-    DataCenter* DC = DataCenter::get_instance();
-    SoundCenter* SC = SoundCenter::get_instance();
-    ImageCenter* IC = ImageCenter::get_instance();
-    FontCenter* FC = FontCenter::get_instance();
+    DataCenter *DC = DataCenter::get_instance();
+    SoundCenter *SC = SoundCenter::get_instance();
+    ImageCenter *IC = ImageCenter::get_instance();
+    FontCenter *FC = FontCenter::get_instance();
     // 设置窗口图标
     game_icon = IC->get(game_icon_img_path);
     al_set_display_icon(display, game_icon);
@@ -165,60 +160,63 @@ void Game::game_init() {
  * @see Game::STATE
  */
 bool Game::game_update() {
-    DataCenter* DC = DataCenter::get_instance();
-    OperationCenter* OC = OperationCenter::get_instance();
-    SoundCenter* SC = SoundCenter::get_instance();
-    static ALLEGRO_SAMPLE_INSTANCE* background = nullptr;
+    DataCenter *DC = DataCenter::get_instance();
+    OperationCenter *OC = OperationCenter::get_instance();
+    SoundCenter *SC = SoundCenter::get_instance();
+    static ALLEGRO_SAMPLE_INSTANCE *background = nullptr;
 
     switch (state) {
-        case STATE::START: {
-            static bool is_played = false;
-            static ALLEGRO_SAMPLE_INSTANCE* instance = nullptr;
-            if (!is_played) {
-                instance = SC->play(game_start_sound_path, ALLEGRO_PLAYMODE_ONCE);
-                DC->level->load_level(1);
-                is_played = true;
-            }
+    case STATE::START: {
+        static bool is_played = false;
+        static ALLEGRO_SAMPLE_INSTANCE *instance = nullptr;
+        if (!is_played) {
+            instance = SC->play(game_start_sound_path, ALLEGRO_PLAYMODE_ONCE);
+            DC->level->load_level(1);
+            is_played = true;
+        }
 
-            if (!SC->is_playing(instance)) {
-                debug_log("<Game> state: change to LEVEL\n");
-                state = STATE::LEVEL;
-            }
-            break;
+        if (!SC->is_playing(instance)) {
+            debug_log("<Game> state: change to LEVEL\n");
+            state = STATE::LEVEL;
         }
-        case STATE::LEVEL: {
-            static bool BGM_played = false;
-            if (!BGM_played) {
-                background = SC->play(background_sound_path, ALLEGRO_PLAYMODE_LOOP);
-                BGM_played = true;
-            }
+        break;
+    }
+    case STATE::LEVEL: {
+        static bool BGM_played = false;
+        if (!BGM_played) {
+            background = SC->play(background_sound_path, ALLEGRO_PLAYMODE_LOOP);
+            BGM_played = true;
+        }
 
-            if (DC->key_state[ALLEGRO_KEY_P] && !DC->prev_key_state[ALLEGRO_KEY_P]) {
-                SC->toggle_playing(background);
-                debug_log("<Game> state: change to PAUSE\n");
-                state = STATE::PAUSE;
-            }
-            if (DC->level->remain_monsters() == 0 && DC->monsters.size() == 0) {
-                debug_log("<Game> state: change to END\n");
-                state = STATE::END;
-            }
-            if (DC->player->HP == 0) {
-                debug_log("<Game> state: change to END\n");
-                state = STATE::END;
-            }
-            break;
+        if (DC->key_state[ALLEGRO_KEY_P] && !DC->prev_key_state[ALLEGRO_KEY_P]) {
+            SC->toggle_playing(background);
+            debug_log("<Game> state: change to PAUSE\n");
+            state = STATE::PAUSE;
         }
-        case STATE::PAUSE: {
-            if (DC->key_state[ALLEGRO_KEY_P] && !DC->prev_key_state[ALLEGRO_KEY_P]) {
-                SC->toggle_playing(background);
-                debug_log("<Game> state: change to LEVEL\n");
-                state = STATE::LEVEL;
-            }
-            break;
+        if (DC->level->remain_monsters() == 0 || DC->player->boss_hp == 0) {
+            debug_log("<Game> state: change to END\n");
+            state = STATE::WIN;
         }
-        case STATE::END: {
-            return false;
+        if (DC->player->HP == 0) {
+            debug_log("<Game> state: change to END\n");
+            state = STATE::LOSE;
         }
+        break;
+    }
+    case STATE::PAUSE: {
+        if (DC->key_state[ALLEGRO_KEY_P] && !DC->prev_key_state[ALLEGRO_KEY_P]) {
+            SC->toggle_playing(background);
+            debug_log("<Game> state: change to LEVEL\n");
+            state = STATE::LEVEL;
+        }
+        break;
+    }
+    case STATE::WIN: {
+        return false;
+    }
+    case STATE::LOSE: {
+        return false;
+    }
     }
     // 如果游戏没有暂停，我们应该继续更新。
     if (state != STATE::PAUSE) {
@@ -240,25 +238,21 @@ bool Game::game_update() {
  * @brief 绘制整个游戏和对象。
  */
 void Game::game_draw() {
-    DataCenter* DC = DataCenter::get_instance();
-    OperationCenter* OC = OperationCenter::get_instance();
-    FontCenter* FC = FontCenter::get_instance();
+    DataCenter *DC = DataCenter::get_instance();
+    OperationCenter *OC = OperationCenter::get_instance();
+    FontCenter *FC = FontCenter::get_instance();
 
     // 首先清空屏幕。
     al_clear_to_color(al_map_rgb(100, 100, 100));
-    if (state != STATE::END) {
+    if (state != STATE::WIN && state != STATE::LOSE) {
         // 背景
         al_draw_bitmap(background, 0, 0, 0);
         if (DC->game_field_length < DC->window_width)
-            al_draw_filled_rectangle(
-                DC->game_field_length, 0,
-                DC->window_width, DC->window_height,
-                al_map_rgb(100, 100, 100));
+            al_draw_filled_rectangle(DC->game_field_length, 0, DC->window_width, DC->window_height,
+                                     al_map_rgb(100, 100, 100));
         if (DC->game_field_length < DC->window_height)
-            al_draw_filled_rectangle(
-                0, DC->game_field_length,
-                DC->window_width, DC->window_height,
-                al_map_rgb(100, 100, 100));
+            al_draw_filled_rectangle(0, DC->game_field_length, DC->window_width, DC->window_height,
+                                     al_map_rgb(100, 100, 100));
         // 用户界面
         if (state != STATE::START) {
             DC->level->draw();
@@ -267,22 +261,28 @@ void Game::game_draw() {
         }
     }
     switch (state) {
-        case STATE::START: {
-        }
-        case STATE::LEVEL: {
-            break;
-        }
-        case STATE::PAUSE: {
-            // 游戏布局覆盖
-            al_draw_filled_rectangle(0, 0, DC->window_width, DC->window_height, al_map_rgba(50, 50, 50, 64));
-            al_draw_text(
-                FC->caviar_dreams[FontSize::LARGE], al_map_rgb(255, 255, 255),
-                DC->window_width / 2., DC->window_height / 2.,
-                ALLEGRO_ALIGN_CENTRE, "GAME PAUSED");
-            break;
-        }
-        case STATE::END: {
-        }
+    case STATE::START: {
+    }
+    case STATE::LEVEL: {
+        break;
+    }
+    case STATE::PAUSE: {
+        // 游戏布局覆盖
+        al_draw_filled_rectangle(0, 0, DC->window_width, DC->window_height, al_map_rgba(50, 50, 50, 64));
+        al_draw_text(FC->caviar_dreams[FontSize::LARGE], al_map_rgb(255, 255, 255), DC->window_width / 2.,
+                     DC->window_height / 2., ALLEGRO_ALIGN_CENTRE, "GAME PAUSED");
+        break;
+    }
+    case STATE::WIN: {
+        al_draw_text(FC->caviar_dreams[FontSize::LARGE], al_map_rgb(255, 255, 255), DC->window_width / 2.,
+                     DC->window_height / 2., ALLEGRO_ALIGN_CENTRE, "YOU WIN!");
+        break;
+    }
+    case STATE::LOSE: {
+        al_draw_text(FC->caviar_dreams[FontSize::LARGE], al_map_rgb(255, 255, 255), DC->window_width / 2.,
+                     DC->window_height / 2., ALLEGRO_ALIGN_CENTRE, "YOU LOSE!");
+        break;
+    }
     }
     al_flip_display();
 }
--- SourceCode/hero.cpp
+++ SourceCode/hero.cpp
@@ -1,7 +1,6 @@
 #include "hero.h"
 #include <string>
 
-#include <allegro5/allegro_primitives.h>
 #include "Level.h"
 #include "Utils.h"
 #include "algif5/algif.h"
@@ -10,6 +9,7 @@
 #include "data/ImageCenter.h"
 #include "shapes/Point.h"
 #include "shapes/Rectangle.h"
+#include <allegro5/allegro_primitives.h>
 
 namespace HeroSetting {
 static constexpr char gif_root_path[50] = "./assets/gif/Hero";
@@ -19,29 +19,26 @@ static constexpr char gif_postfix[][10] = {
     "front",
     "back",
 };
-}  // namespace HeroSetting
+} // namespace HeroSetting
 
 void Hero::init() {
     for (size_t i = 0; i < static_cast<size_t>(Hstate::max); i++) {
         char buffer[50];
-        sprintf(buffer, "%s/dragonite_%s.gif", HeroSetting::gif_root_path, HeroSetting::gif_postfix[static_cast<int>(i)]);
+        sprintf(buffer, "%s/dragonite_%s.gif", HeroSetting::gif_root_path,
+                HeroSetting::gif_postfix[static_cast<int>(i)]);
         gifPath[static_cast<Hstate>(i)] = std::string(buffer);
     }
-    GIFCenter* GIFC = GIFCenter::get_instance();
-    ALGIF_ANIMATION* gif = GIFC->get(gifPath[Hstate::front]);
-    DataCenter* DC = DataCenter::get_instance();
-    shape.reset(new Rectangle(DC->window_width / 2,
-                              DC->window_height / 2,
-                              DC->window_width / 2 + gif->width,
+
+    GIFCenter *GIFC = GIFCenter::get_instance();
+    ALGIF_ANIMATION *gif = GIFC->get(gifPath[Hstate::front]);
+    DataCenter *DC = DataCenter::get_instance();
+    shape.reset(new Rectangle(DC->window_width / 2, DC->window_height / 2, DC->window_width / 2 + gif->width,
                               DC->window_height / 2 + gif->height));
 }
 
 void Hero::draw() {
-    GIFCenter* GFIC = GIFCenter::get_instance();
-    ALGIF_ANIMATION* gif = GFIC->get(gifPath[state]);
+    GIFCenter *GFIC = GIFCenter::get_instance();
+    ALGIF_ANIMATION *gif = GFIC->get(gifPath[state]);
 
-    algif_draw_gif(
-        gif,
-        shape->center_x() - gif->width / 2,
-        shape->center_y() - gif->height / 2, 0);
+    algif_draw_gif(gif, shape->center_x() - gif->width / 2, shape->center_y() - gif->height / 2, 0);
 }
